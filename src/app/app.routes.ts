77978ce9cd4e1d339d78f0f14834cb63039f79bf
iurii-kyrylenko <yuriproject@gmail.com>
2016-07-02 18:30:23 +0300

    Updated to rc4

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Task1Component } from './task-1/task1.component';
 import { Task2Component } from './task-2/task2.component';
 
 export const routes: RouterConfig = [
-    { path: '', redirectTo: '/task1', terminal: true },
+    { path: '', redirectTo: '/task1', pathMatch: 'full' },
     { path: 'task1', component: Task1Component },
     { path: 'task2', component: Task2Component }
 ];
